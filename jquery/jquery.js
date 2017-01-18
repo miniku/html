33695966f104731272dc1f1bdf72fc93ebbc2681
miniku <nathanhartman1@gmail.com>
2017-01-17 20:51:43 -0800

    jQuery Day1

---
+++ jquery/jquery.js
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Test</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</head>
+<body>
+<title>
+  Test
+</title>
+<link href="../site.css" rel="stylesheet" href="styles/style.css" />
+</head>
+<header>
+
+    <nav>
+      <ul class="nav navbar-nav">
+        <li><a href="../index.html">Home</a></li>
+        <li><a href="../bootstrap/bootstrap.html">Bootstrap</a></li>
+        <li><a href="test.html">Test</a></li>
+        <li><a href="jquery.js">jQuery</a></li>
+        <li><a href="../slideshow/slideshow.html">Links</a></li>
+      </ul>
+    </nav>
+
+  </header>
+  <body>
+
+      <script scr="/jquery.min.js"></script>
+      <script> src="../js/bootstrap.min.js"></script>
+      <script
+			  src="//code.jquery.com/jquery-3.1.1.min.js"
+			  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+			  crossorigin="anonymous"></script>
+  </body>
+
+</html>
